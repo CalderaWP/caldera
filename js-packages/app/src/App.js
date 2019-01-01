2c721542df4a04f2ba68f6ff7c09ce80f9d4d9ea
Josh Pollock <josh@calderawp.com>
2019-01-01 14:21:53 -0500

    configure app proxy, with some failed lando setup

--- js-packages/app/src/App.js
+++ js-packages/app/src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Header } from '@caldera-labs/components';
-import { HomePage } from './features/home/home-page';
+//import { Header } from '@caldera-labs/components';
+//import { HomePage } from './features/home/home-page';
 
 export class App extends React.Component {
+
+	componentDidMount(){
+		fetch('/wp-json/caldera-api/v1/' ).then(r => r.json() )
+			.then( r => console.log(r))
+			.catch( e => console.log(e))
+	}
 	render() {
 		return (
 			<div>
-				<Header
-					styles={{
-						header: {
-							backgroundColor: '#ff7e30',
-							color: '#fff'
-						}
-					}}
-				>
-					Caldera Pro
-				</Header>
-				<HomePage />
+				The Ap@p
 			</div>
 		);
 	}
