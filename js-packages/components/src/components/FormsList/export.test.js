1497ffa8d3f5ec04121535002fea39d1e118bd70
Josh Pollock <josh@calderawp.com>
2019-01-03 14:27:27 -0500

    remove form list for now

--- js-packages/components/src/components/FormsList/export.test.js
+++
@@ -1,8 +0,0 @@
-import FormListComponents from './index';
-import { snapshotObjectKeysAndTypes } from '../../../../../../Downloads/caldera-admin-master/src/testUtil/snapshotObjectKeysAndTypes';
-
-describe('Form List components export', () => {
-	it('matches snapshot', () => {
-		snapshotObjectKeysAndTypes(FormListComponents);
-	});
-});
