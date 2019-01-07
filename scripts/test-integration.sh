aeef50ff0f9ee56044f067c528600803de884f91
Josh Pollock <josh@calderawp.com>
2019-01-07 06:53:22 -0500

    refacotr integration test runner

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-composer test:core || { echo 'calderawp/core integration tests failed' ; exit 1; }
+composer test:core || { echo 'calderawp/core unit (integrationish) tests failed' ; exit 1; }
+composer test:integration-core || { echo 'calderawp/core integration tests failed' ; exit 1; }
 composer test:wordpress || { echo 'WordPress tests failed' ; exit 1; }
 
