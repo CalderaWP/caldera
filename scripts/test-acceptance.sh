8734a9026261358086b09dad95e05c5f72a948e1
Josh Pollock <josh@calderawp.com>
2019-01-07 18:27:26 -0500

    Make API request procesor callback work

--- scripts/test-acceptance.sh
+++ scripts/test-acceptance.sh
@@ -2,9 +2,9 @@
 cd php-packages;
 pwd
 cd http
-composer test:acceptance
+composer test:acceptance || { echo 'calderawp/http integration tests failed' ; exit 1; }
 cd ../../
 
 cd mu-plugins/plugins/wordpress-plugin/
 pwd
-composer test:acceptance
+composer test:acceptance || { echo 'mu-plugins/plugins/wordpress-plugin integration tests failed' ; exit 1; }
