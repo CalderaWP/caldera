af025ec33758e78a5e18d7eed098cbc2582b185d
Josh Pollock <josh@calderawp.com>
2019-01-07 07:08:52 -0500

    start processor types, fix pre-processing

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 composer test:core || { echo 'calderawp/core unit (integrationish) tests failed' ; exit 1; }
+composer test:integration-forms || { echo 'calderawp/forms integration tests failed' ; exit 1; }
 composer test:integration-core || { echo 'calderawp/core integration tests failed' ; exit 1; }
 composer test:wordpress || { echo 'WordPress tests failed' ; exit 1; }
 
