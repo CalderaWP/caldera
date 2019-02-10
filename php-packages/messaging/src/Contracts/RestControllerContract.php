3300e473c5b830e774d33fb2ee1ffdee624a1e07
Josh Pollock <josh@calderawp.com>
2019-02-09 21:11:35 -0500

    add a rest api controller for a data source

--- php-packages/messaging/src/Contracts/RestControllerContract.php
+++ php-packages/messaging/src/Contracts/RestControllerContract.php
@@ -4,19 +4,9 @@
 namespace calderawp\caldera\Messaging\Contracts;
 
 
-use calderawp\interop\Contracts\HttpRequestContract as Request;
-use calderawp\interop\Contracts\HttpResponseContract as Response;
-
-interface RestControllerContract
+interface RestControllerContract extends \calderawp\caldera\restApi\Contracts\RestControllerContract
 {
-	public function get(Request $request ): Response;
-	public function create(Request$request ): Response;
-	public function update(Request$request ): Response;
-	public function delete(Request$request): Response;
-	public function list(Request$request): Response;
-	public function annonymize(Request$request): Response;
-	public function authorizeRequest(Request$request):bool ;
-	public function search(Request$request):bool ;
+
 
 
 }
